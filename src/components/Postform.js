b791988d21832a602bd7850e993a34f85ff752e0
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2018-11-15 12:02:48 -0700

    initial commit

---
+++ src/components/Postform.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Postform extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Add Post</h1>       
+        </div>
+        )
+    }
+}
