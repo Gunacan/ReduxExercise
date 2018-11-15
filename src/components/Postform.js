982371bf4f2cc4d9460616b8bec5786ced058fa8
Onder Gunacan <ondergunacan@Onders-MacBook-Air.local>
2018-11-15 14:34:42 -0700

    form submit working

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
 
 export default class Postform extends Component {
+
+    state = {
+        title: '',
+        body: ''
+    }
+
+    onChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+
+        const postData = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: "POST",
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(postData)
+        }) 
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
+
     render() {
         return (
         <div>
-            <h1>Add Post</h1>       
+            <h1>Add Post</h1>   
+            <form onSubmit={this.onSubmit}>
+                <div>
+                    <label>Title</label>
+                    <br/>
+                    <input type="text" name='title' value={this.state.title} onChange={this.onChange} />
+                </div>
+                <br/>
+                <div>
+                    <label>Body</label>
+                    <br/>
+                    <textarea name="body" id="body" cols="30" rows="10" value={this.state.body} onChange={this.onChange} ></textarea>
+                </div>
+                <br/>
+                <button type='submit'>Submit</button>
+            </form>    
         </div>
         )
     }
